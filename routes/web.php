d3306d78fd4c6bdcb4ee8de4219f9e1fb3ac3b63
Md Arifur Rahman <arif.aust.eng@gmail.com>
2023-11-19 13:07:10 +0100

    assignment 8 done

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/profile/{id}', [ProfileController::class, 'index']);
 
 
